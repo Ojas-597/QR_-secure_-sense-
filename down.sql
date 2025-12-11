67187f80339aaf27cd4a7adab2206882eb853842
Ojaswita Ranjit Desai <ojaswitadesai@gmail.com>
2025-12-11 17:50:53 +0530

    Add files via upload

---
+++ down.sql
@@ -0,0 +1,8 @@
+
+DROP INDEX idx_threat_removals_session_id;
+DROP INDEX idx_analytics_events_event_type;
+DROP INDEX idx_analytics_events_session_id;
+DROP INDEX idx_quiz_sessions_session_id;
+DROP TABLE threat_removals;
+DROP TABLE analytics_events;
+DROP TABLE quiz_sessions;
